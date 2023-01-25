2a4ebc33772960200e9a481db944f9e423e0e7a8
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2023-01-25 12:44:03 +0200

    c

--- src/pages/cabinet.tsx
+++ src/pages/cabinet.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { withAuth } from '@/hoc/RequiredAuth'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import user, { selectUserInfo } from '../redux/slices/user'
+import { selectUserInfo } from '../redux/slices/user'
 import { NextPage } from 'next'
-
+import Image from 'next/image'
+import noteSvg from '../assets/icons/NotePencil.svg'
+import lockSvg from '../assets/icons/LockKey.svg'
+import receiptSvg from '../assets/icons/Receipt.svg'
+import markedSvg from '../assets/icons/BookmarkSimple.svg'
+import cl from '../styles/cabinet.module.scss'
 const Cabinet: NextPage = () => {
 	const dispatch = useAppDispatch()
 	const userInfo = useAppSelector(selectUserInfo)
@@ -14,7 +19,31 @@ const Cabinet: NextPage = () => {
 				<div className='page_coordinator'>
 					Главная | <span>Личный кабинет</span>
 				</div>
-				<div className='cabinet_body'></div>
+				<div className={cl.cabinet_tabs}>
+					<button className={cl.cabinet_tablink}>
+						<span></span>Изменить данные
+					</button>
+					<button className={cl.cabinet_tablink}>
+						<span></span>
+						Изменить данные
+					</button>
+					<button className={cl.cabinet_tablink}>
+						<span></span>Second tab
+					</button>
+				</div>
+				<div className={cl.cabinet_tabcontent}>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+						dignissim maximus enim, nec dictum diam pellentesque nec. Nulla
+						facilisis, massa ut egestas bibendum, ex libero varius nisl, a
+						aliquet dui ante sed purus. Donec vitae ullamcorper lacus, ut
+						vehicula metus. Duis molestie ipsum quis nisl ultrices feugiat. Nam
+						at purus ut ligula ultrices aliquet vitae nec eros. Aenean
+						condimentum tellus eu urna tincidunt volutpat. Nunc bibendum quam ut
+						congue dignissim. Aliquam congue velit at tortor consequat cursus ut
+						ut diam. Fusce iaculis auctor purus.
+					</p>
+				</div>
 				<h1>Users cabinet</h1>
 				{!userInfo || userInfo === null
 					? 'no info'
