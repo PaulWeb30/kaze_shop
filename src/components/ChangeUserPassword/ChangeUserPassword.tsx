a76edd4bdfec12f72f942f04f134893ebe614249
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2023-01-28 14:56:29 +0200

    cfwe

--- src/components/ChangeUserPassword/ChangeUserPassword.tsx
+++ src/components/ChangeUserPassword/ChangeUserPassword.tsx
@@ -1,7 +1,92 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { ChangeUserPasswordDto } from '@/types/auth'
+import { ChangeUserPasswordShema } from '@/utils/validation'
+import cl from '../../styles/cabinet.module.scss'
+import Image from 'next/image'
+import hidenIcon from '../../assets/icons/close_eye.svg'
+import showIcon from '../../assets/icons/show_eye.svg'
 const ChangeUserPassword = () => {
-	return <div>ChangePassword</div>
+	const [passwordShown, setPasswordShown] = useState(false)
+	const [confirmPasswordShown, setConfirmPasswordShown] = useState(false)
+	const changeUserPasswordForm = useForm<ChangeUserPasswordDto>({
+		mode: 'onChange',
+		resolver: yupResolver(ChangeUserPasswordShema),
+	})
+	const onSubmit = async (dto: ChangeUserPasswordDto) => {
+		console.log(dto)
+	}
+
+	const togglePasswordShown = () => {
+		setPasswordShown(prev => !prev)
+	}
+
+	const toggleConfirmPasswordShown = () => {
+		setConfirmPasswordShown(prev => !prev)
+	}
+
+	return (
+		<form
+			onSubmit={changeUserPasswordForm.handleSubmit(onSubmit)}
+			className={cl.cabinet_tabcontent}
+		>
+			<div className={cl.cabinet_field}>
+				<label className='auth_label' htmlFor='email'>
+					Введите пароль
+				</label>
+				<div className='auth_input'>
+					<input
+						placeholder='Введите пароль'
+						type={passwordShown ? 'text' : 'password'}
+						{...changeUserPasswordForm.register('password')}
+					/>
+					<div onClick={togglePasswordShown} className='auth_hidden-icon'>
+						<Image
+							src={passwordShown ? showIcon : hidenIcon}
+							alt='show password icon'
+							width={24}
+							height={24}
+						/>
+					</div>
+				</div>
+				<span className='auth_error'>
+					{changeUserPasswordForm.formState.errors.password &&
+						changeUserPasswordForm.formState.errors.password.message}
+				</span>
+			</div>
+			<div className={cl.cabinet_field}>
+				<label className='auth_label' htmlFor='email'>
+					Повторите пароль
+				</label>
+				<div className='auth_input'>
+					<input
+						placeholder='Повторите пароль'
+						type={confirmPasswordShown ? 'text' : 'password'}
+						{...changeUserPasswordForm.register('confirmPassword')}
+					/>
+					<div
+						onClick={toggleConfirmPasswordShown}
+						className='auth_hidden-icon'
+					>
+						<Image
+							src={confirmPasswordShown ? showIcon : hidenIcon}
+							alt='show password icon'
+							width={24}
+							height={24}
+						/>
+					</div>
+				</div>
+				<span className='auth_error'>
+					{changeUserPasswordForm.formState.errors.confirmPassword &&
+						changeUserPasswordForm.formState.errors.confirmPassword.message}
+				</span>
+			</div>
+			<button className={cl.cabinet_btn} type='submit'>
+				Cохранить
+			</button>
+		</form>
+	)
 }
 
 export default ChangeUserPassword
--- src/components/screens/Cabinet/CabinetTabs.tsx
+++ src/components/screens/Cabinet/CabinetTabs.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import cl from '../../../styles/cabinet.module.scss'
-import Link from 'next/link'
 import ChangeUserInfo from '@/components/ChangeUserInfo/ChangeUserInfo'
+import ChangeUserPassword from '@/components/ChangeUserPassword/ChangeUserPassword'
 const CabinetTabs: FC<{
 	toggleTab: React.MouseEventHandler<HTMLButtonElement>
 }> = ({ toggleTab }) => {
@@ -18,15 +18,20 @@ const CabinetTabs: FC<{
 					></span>
 					Изменить данные
 				</button>
-				<ChangeUserInfo />
+				{/* <ChangeUserInfo /> */}
 			</div>
 			<div className={cl.cabinet_tab}>
-				<button className={cl.cabinet_tablink}>
+				<button
+					data-tabindex={2}
+					onClick={toggleTab}
+					className={cl.cabinet_tablink}
+				>
 					<span
 						className={`${cl.cabinet_icon} ${cl.cabinet_icon_change}`}
 					></span>
 					Изменить пароль
 				</button>
+				<ChangeUserPassword />
 			</div>
 			<div className={cl.cabinet_tab}>
 				<button className={cl.cabinet_tablink}>
