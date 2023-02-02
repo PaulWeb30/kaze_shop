157b4a6dc01aced148a7eecf434b409bae39592e
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2023-02-02 12:24:07 +0200

    c

--- src/components/ChangeUserInfo/ChangeUserInfo.tsx
+++ src/components/ChangeUserInfo/ChangeUserInfo.tsx
@@ -53,6 +53,7 @@ const ChangeUserInfo = () => {
 			}
 		}
 	}
+
 	return (
 		<>
 			<form
--- src/components/screens/Cabinet/Cabinet.tsx
+++ src/components/screens/Cabinet/Cabinet.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import { NextPage } from 'next'
+import React, { FC } from 'react'
+import { Api } from '@/services'
+import { useAppDispatch } from '@/redux/hooks'
+import { useRouter } from 'next/router'
+import { addUserInfo } from '@/redux/slices/user'
 import Link from 'next/link'
 import CabinetTabs from '@/components/screens/Cabinet/CabinetTabs'
-const Cabinet: NextPage = () => {
+const Cabinet: FC = () => {
+	const dispatch = useAppDispatch()
+	const router = useRouter()
+	React.useEffect(() => {
+		const fetchUserData = async () => {
+			try {
+				const data = await Api().user.getMe()
+				if (data.user) {
+					dispatch(addUserInfo(data.user))
+				}
+			} catch (e) {
+				router.push('/404')
+			}
+		}
+		fetchUserData()
+	}, [])
 	return (
 		<main className='content'>
 			<div className='container'>
